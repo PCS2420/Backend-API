d530701987f7c5f8bcc397387ae43d2014bb2b9e
gabrieltnishimura <gabrieltnishimura@gmail.com>
2015-11-15 00:39:41 -0200

    Capas para livros de direito + cron menos frequente
    30/30 min

--- config/schedule.js
+++ config/schedule.js
@@ -2,15 +2,16 @@ module.exports.schedule = {
 	sailsInContext : true, //If sails is not as global and you want to have it in your tas
 	tasks : {
 	  firstTask : {
-		cron : "*/1 * * * *", // uma vez a cada minuto
+		cron : "*/30 * * * *", // uma vez a cada minuto
 		task : function (context)
 		{
-			var N_DIAS = 1, N_MINUTOS = 1;
+			var N_DIAS = 1, N_MINUTOS = 30; // deleta todas de trinta minutos atras
 			
 			var N_DIAS_ATRAS = new Date();
 			var N_MINUTOS_ATRAS = new Date(new Date() - N_MINUTOS * 60000);
 			N_DIAS_ATRAS.setDate(N_DIAS_ATRAS.getDate() - N_DIAS); // atualiza todas imagens travadas a cada N dias
 			
+			//Troque por N_DIAS_ATRAS se quiser realizar por dias, e por N_MINUTOS_ATRAS se quiser CRONAR por minutos
 			console.log("[CRON] Tarefa que atualiza imagens velhas do estado EmAndamento.");
 			Imagem.update({updatedAt: {$lt : N_MINUTOS_ATRAS}, estado: "EmAndamento"}, {estado: "Aberto"}).exec(function afterwards(err, res) {
 				if (err) {
